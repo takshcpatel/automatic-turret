7b62392317805b336251c4a56bd0af49fa6547bd
Taksh Patel <takshcpatel@gmail.com>
2025-12-16 09:29:02 +0530

    Added camera to GUI and improved
    added camera feed to gui ( with cv2 processing )

--- Python Code/controller.py
+++ Python Code/controller.py
@@ -1,8 +1,70 @@
-from PyQt5.QtCore import QObject
+from PyQt5.QtCore import QThread, pyqtSignal
 
-class Controller(QObject):
+from picamera2 import Picamera2
+from libcamera import Transform
+
+import cv2
+import numpy as np
+import time
+
+class CameraWorker(QThread):
+    frame_ready = pyqtSignal(object)
+    status = pyqtSignal(str)
+
     def __init__(self):
         super().__init__()
+        self.running = False
+        self.picam2 = None
 
-    def handle_button_click(self):
-        print("Button clicked from Controller!")
+    def run(self):
+        self.picam2 = Picamera2()
+        self.picam2.configure(
+            self.picam2.create_preview_configuration(
+                main={"format": "BGR888", "size": (640, 480)},
+                transform=Transform(rotation=180)
+            )
+        )
+
+        self.picam2.start()
+        time.sleep(1.5)
+
+        self.picam2.set_controls({"AfMode": 2})
+        self.status.emit("[!] Camera started")
+
+        self.running = True
+
+        while self.running:
+            frame = self.picam2.capture_array()
+
+            h, w, _ = frame.shape
+            screen_center = (w // 2, h // 2)
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+            lower_green = np.array([30, 50, 30])
+            upper_green = np.array([90, 255, 220])
+
+            mask = cv2.inRange(hsv, lower_green, upper_green)
+            mask = cv2.GaussianBlur(mask, (5, 5), 0)
+            mask = cv2.erode(mask, None, iterations=2)
+            mask = cv2.dilate(mask, None, iterations=2)
+
+            contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            if contours:
+                largest = max(contours, key=cv2.contourArea)
+                if cv2.contourArea(largest) > 500:
+                    x, y, bw, bh = cv2.boundingRect(largest)
+                    obj_center = (x + bw // 2, y + bh // 2)
+
+                    cv2.rectangle(frame, (x, y), (x + bw, y + bh), (0, 255, 0), 2)
+                    cv2.circle(frame, obj_center, 5, (0, 0, 255), -1)
+                    cv2.line(frame, screen_center, obj_center, (255, 0, 0), 2)
+
+            self.frame_ready.emit(frame)
+            time.sleep(0.01)
+
+        self.picam2.stop()
+        self.status.emit("[!] Camera stopped")
+
+    def stop(self):
+        self.running = False
--- Python Code/main.py
+++ Python Code/main.py
@@ -2,12 +2,18 @@ from PyQt5.QtWidgets import (
     QApplication,
     QWidget,
     QVBoxLayout,
+    QHBoxLayout,
     QPushButton,
     QPlainTextEdit,
     QCheckBox,
-    QLineEdit
+    QLineEdit,
+    QLabel
 )
+from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import Qt
+from controller import CameraWorker
+import cv2
 import sys
 import serial
 
@@ -15,26 +21,46 @@ class Window(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.camera = CameraWorker()
+        self.camera.frame_ready.connect(self.on_new_frame)
+        self.camera.status.connect(self.log)
+
+        self.camera.start()
+
         # --- UI ---#
         self.setWindowTitle("New Person, Same Old Mistakes - Tame Impala")
         self.resize(960, 640)
+        self.setStyleSheet("background-color: #2b2b2b; color: white; font-family: Arial; font-size: 14px;")
+
         
         self.BTN_Connect = QPushButton("Connect")
+        self.BTN_Connect.setFixedSize(100, 40)
+        self.BTN_Connect.setStyleSheet("background-color: #4CAF50; color: white; border: none; border-radius: 5px;")
 
-        self.Console = QPlainTextEdit()
+        self.video_label = QLabel()                                                                                              ## video display
+        self.video_label.setFixedSize(640, 480)
+        self.video_label.setStyleSheet("background-color: black;")
+
+        self.Console = QPlainTextEdit()                                                                                          ## console 
+        self.Console.setFixedSize(300, 500)
         self.Console.setReadOnly(True)
         self.command_input = QLineEdit()
         self.command_input.setPlaceholderText("GCode I/P ...")
 
 
-        layout_main = QVBoxLayout()
-        layout_console = QVBoxLayout()
+        layout_main = QHBoxLayout()                                                                                              ## main Layout
 
-        self.checkbox_lvl1 = QCheckBox("Notification Level [!]")
+        layout_console = QVBoxLayout()
+        layout_console.setContentsMargins(10, 0, 0, 0)
+        layout_console.setSpacing(10)
+
+        self.checkbox_lvl1 = QCheckBox("Notification Level [!]")                                                                 ## checkboxes
         self.checkbox_lvl2 = QCheckBox("Notification Level [!!]")
         self.checkbox_lvl3 = QCheckBox("Notification Level [!!!]")
 
-        layout_console.addWidget(self.checkbox_lvl1)
+        layout_main.addWidget(self.video_label)                                                                                  
+
+        layout_console.addWidget(self.checkbox_lvl1)                                                                             ## add widgets
         layout_console.addWidget(self.checkbox_lvl2)
         layout_console.addWidget(self.checkbox_lvl3)
 
@@ -42,7 +68,7 @@ class Window(QWidget):
         layout_console.addWidget(self.Console)
         layout_console.addWidget(self.command_input)
 
-        self.command_input.returnPressed.connect(self.send_command)
+        self.command_input.returnPressed.connect(self.send_command)                                                              ## connect interactives 
         layout_main.addLayout(layout_console)
         self.setLayout(layout_main)
 
@@ -85,6 +111,29 @@ class Window(QWidget):
 
         self.Console.appendPlainText(text)
 
+    def on_new_frame(self, frame):
+        h, w, ch = frame.shape
+        bytes_per_line = ch * w
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        qt_image = QImage(
+            rgb_frame.data,
+            w,
+            h,
+            bytes_per_line,
+            QImage.Format_RGB888
+        )
+
+        pixmap = QPixmap.fromImage(qt_image)
+
+        self.video_label.setPixmap(
+            pixmap.scaled(
+                self.video_label.size(),
+                Qt.KeepAspectRatio,
+                Qt.SmoothTransformation
+            )
+        )
+
     def connect_arduino(self):
         try:
             self.ser = serial.Serial("/dev/ttyACM0", 115200, timeout=0)
@@ -103,6 +152,11 @@ class Window(QWidget):
             except Exception as e:
                 self.log(f"Read error: {e}")
 
+    def closeEvent(self, event):
+        if self.camera.isRunning():
+            self.camera.stop()
+            self.camera.wait()
+        event.accept()
 
 app = QApplication(sys.argv)
 window = Window()
