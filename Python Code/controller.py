855872969f1e729f505ef45df1ac0dfc5f037230
Taksh Patel <takshcpatel@gmail.com>
2025-12-19 01:51:48 +0530

    added deadzone slider
    added deadzone slider to gui and made it update on opencv draw frame to show user the outline of the deadzone.

--- Python Code/controller.py
+++ Python Code/controller.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot
 
 from picamera2 import Picamera2
 from libcamera import Transform
@@ -7,30 +7,32 @@ import cv2
 import numpy as np
 import time
 
+
 class CameraWorker(QThread):
     frame_ready = pyqtSignal(object)
+    err_signal = pyqtSignal(int, int)
     status = pyqtSignal(str)
 
     def __init__(self):
         super().__init__()
         self.running = False
         self.picam2 = None
+        self.SLIDER_deadzone = 10
 
     def run(self):
         self.picam2 = Picamera2()
         self.picam2.configure(
             self.picam2.create_preview_configuration(
                 main={"format": "BGR888", "size": (640, 480)},
-                transform=Transform(rotation=180)
+                transform=Transform(rotation=180),
             )
         )
-
+        
         self.picam2.start()
         time.sleep(1.5)
-
+        
         self.picam2.set_controls({"AfMode": 2})
         self.status.emit("[!] Camera started")
-
         self.running = True
 
         while self.running:
@@ -39,17 +41,25 @@ class CameraWorker(QThread):
 
             h, w, _ = frame.shape
             screen_center = (w // 2, h // 2)
+            
+            err_x, err_y = 0, 0
+
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-            lower_green = np.array([30, 50, 30])
-            upper_green = np.array([90, 255, 220])
-
-            mask = cv2.inRange(hsv, lower_green, upper_green)
+            lower_yellow = np.array([20, 100, 100])
+            upper_yellow = np.array([30, 255, 255])
+            
+            mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
             mask = cv2.GaussianBlur(mask, (5, 5), 0)
             mask = cv2.erode(mask, None, iterations=2)
             mask = cv2.dilate(mask, None, iterations=2)
+            
+            contours, _ = cv2.findContours(
+                mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+            )
 
-            contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.rectangle(frame, (w//2 - self.SLIDER_deadzone, h//2 - self.SLIDER_deadzone),
+                                  (w//2 + self.SLIDER_deadzone, h//2 + self.SLIDER_deadzone), (255, 255, 0), 2)
+            cv2.circle(frame, screen_center, 5, (255, 0, 255), -1)
 
             if contours:
                 largest = max(contours, key=cv2.contourArea)
@@ -57,15 +67,31 @@ class CameraWorker(QThread):
                     x, y, bw, bh = cv2.boundingRect(largest)
                     obj_center = (x + bw // 2, y + bh // 2)
 
+                    err_x = (obj_center[0] - screen_center[0])
+                    err_y = (obj_center[1] - screen_center[1])
+
+                    if abs(err_x) < self.SLIDER_deadzone:
+                        err_x = 0
+                    if abs(err_y) < self.SLIDER_deadzone:
+                        err_y = 0
+
+                    
+
                     cv2.rectangle(frame, (x, y), (x + bw, y + bh), (0, 255, 0), 2)
                     cv2.circle(frame, obj_center, 5, (0, 0, 255), -1)
                     cv2.line(frame, screen_center, obj_center, (255, 0, 0), 2)
-
+            
+            self.err_signal.emit(err_x, err_y)
             self.frame_ready.emit(frame)
             time.sleep(0.01)
 
         self.picam2.stop()
         self.status.emit("[!] Camera stopped")
+    
+    @pyqtSlot(int)
+    def update_deadzone(self, value):
+        self.SLIDER_deadzone = value
+        self.status.emit(f"[!] Deadzone updated to: {value}")
 
     def stop(self):
         self.running = False
--- Python Code/main.py
+++ Python Code/main.py
@@ -2,65 +2,92 @@ from PyQt5.QtWidgets import (
     QApplication,
     QWidget,
     QVBoxLayout,
+    QGroupBox,
     QHBoxLayout,
     QPushButton,
     QPlainTextEdit,
     QCheckBox,
     QLineEdit,
-    QLabel
+    QLabel,
+    QSlider,
 )
+
 from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtCore import QTimer
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, pyqtSignal
 from controller import CameraWorker
 import cv2
 import sys
 import serial
 
+
 class Window(QWidget):
+    deadzone_value = pyqtSignal(int)
+
     def __init__(self):
         super().__init__()
 
         self.camera = CameraWorker()
         self.camera.frame_ready.connect(self.on_new_frame)
+        self.camera.err_signal.connect(self.send_tracking)
         self.camera.status.connect(self.log)
 
         self.camera.start()
+        self.deadzone_value.connect(self.camera.update_deadzone)
 
         # --- UI ---#
         self.setWindowTitle("New Person, Same Old Mistakes - Tame Impala")
         self.resize(960, 640)
-        self.setStyleSheet("background-color: #2b2b2b; color: white; font-family: Arial; font-size: 14px;")
+        self.setStyleSheet(
+            "background-color: #2b2b2b; color: white; font-family: Arial; font-size: 14px;"
+        )
 
-        
         self.BTN_Connect = QPushButton("Connect")
         self.BTN_Connect.setFixedSize(100, 40)
-        self.BTN_Connect.setStyleSheet("background-color: #4CAF50; color: white; border: none; border-radius: 5px;")
+        self.BTN_Connect.setStyleSheet(
+            "background-color: #4CAF50; color: white; border: none; border-radius: 5px;"
+        )
 
-        self.video_label = QLabel()                                                                                              ## video display
+        self.SLIDER_Deadzone = QSlider(Qt.Horizontal)
+        self.SLIDER_Deadzone.setRange(0, 50)
+        self.SLIDER_Deadzone.setSingleStep(1)
+        self.SLIDER_Deadzone.setValue(10)
+
+        self.SLIDER_Deadzone.valueChanged.connect(self.deadzone_value.emit)
+
+        self.deadzone_group = QGroupBox("Deadzone")
+        deadzone_layout = QVBoxLayout()
+        deadzone_layout.addWidget(self.SLIDER_Deadzone)
+        self.deadzone_group.setLayout(deadzone_layout)
+        self.deadzone_group.setFixedSize(500, 70)
+
+        self.video_label = QLabel()  ## video display
         self.video_label.setFixedSize(640, 480)
         self.video_label.setStyleSheet("background-color: black;")
 
-        self.Console = QPlainTextEdit()                                                                                          ## console 
+        self.Console = QPlainTextEdit()  ## console
         self.Console.setFixedSize(300, 500)
         self.Console.setReadOnly(True)
         self.command_input = QLineEdit()
         self.command_input.setPlaceholderText("GCode I/P ...")
 
-
-        layout_main = QHBoxLayout()                                                                                              ## main Layout
+        layout_main = QHBoxLayout()  ## main Layout
 
         layout_console = QVBoxLayout()
         layout_console.setContentsMargins(10, 0, 0, 0)
         layout_console.setSpacing(10)
 
-        self.checkbox_lvl1 = QCheckBox("Notification Level [!]")                                                                 ## checkboxes
+        layout_main_sub = QVBoxLayout()
+
+        self.checkbox_lvl1 = QCheckBox("Notification Level [!]")  ## checkboxes
         self.checkbox_lvl2 = QCheckBox("Notification Level [!!]")
         self.checkbox_lvl3 = QCheckBox("Notification Level [!!!]")
 
-        layout_main.addWidget(self.video_label)                                                                                  
+        layout_main_sub.addWidget(self.video_label)
+        layout_main_sub.addWidget(self.deadzone_group)
 
-        layout_console.addWidget(self.checkbox_lvl1)                                                                             ## add widgets
+        layout_main.addLayout(layout_main_sub)
+
+        layout_console.addWidget(self.checkbox_lvl1)  ## add widgets
         layout_console.addWidget(self.checkbox_lvl2)
         layout_console.addWidget(self.checkbox_lvl3)
 
@@ -68,7 +95,9 @@ class Window(QWidget):
         layout_console.addWidget(self.Console)
         layout_console.addWidget(self.command_input)
 
-        self.command_input.returnPressed.connect(self.send_command)                                                              ## connect interactives 
+        self.command_input.returnPressed.connect(
+            self.send_command
+        )  ## connect interactives
         layout_main.addLayout(layout_console)
         self.setLayout(layout_main)
 
@@ -83,6 +112,15 @@ class Window(QWidget):
         self.timer.timeout.connect(self.read_serial)
 
         self.BTN_Connect.clicked.connect(self.connect_arduino)
+
+    def send_tracking(self, err_x, err_y):
+        if self.ser and self.ser.is_open:
+            try:
+                self.log(f"[ TRACK CMD ] => X:{err_x} Y:{err_y}")
+            except Exception as e:
+                self.log(f"[!!] Error while sending tracking command :- {e}")
+        else:
+            self.log("[!!!] Serial port not connected.")
 
     def send_command(self):
         command = self.command_input.text().strip()
@@ -102,7 +140,7 @@ class Window(QWidget):
     def log(self, text):
         if text.startswith("[!!!]") and not self.checkbox_lvl3.isChecked():
             return
-        
+
         if text.startswith("[!!]") and not self.checkbox_lvl2.isChecked():
             return
 
@@ -116,21 +154,13 @@ class Window(QWidget):
         bytes_per_line = ch * w
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        qt_image = QImage(
-            rgb_frame.data,
-            w,
-            h,
-            bytes_per_line,
-            QImage.Format_RGB888
-        )
+        qt_image = QImage(rgb_frame.data, w, h, bytes_per_line, QImage.Format_RGB888)
 
         pixmap = QPixmap.fromImage(qt_image)
 
         self.video_label.setPixmap(
             pixmap.scaled(
-                self.video_label.size(),
-                Qt.KeepAspectRatio,
-                Qt.SmoothTransformation
+                self.video_label.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation
             )
         )
 
@@ -157,6 +187,7 @@ class Window(QWidget):
             self.camera.stop()
             self.camera.wait()
         event.accept()
+
 
 app = QApplication(sys.argv)
 window = Window()
